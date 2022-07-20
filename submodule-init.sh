a73ff05580f0db279e538babb1258ce8ba32f282
Artem Dmytriv <artemdmytriv@gmail.com>
2022-07-20 13:46:37 +0300

    Add feature (working dir) to submodule init script

--- submodule-init.sh
+++ submodule-init.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-git submodule update --init
+if [[ ! -f ".curworkdir" && $# -gt 0 ]]; then
+    echo $1 > .curworkdir
+fi
+
+arg=""
+if [ -f ".curworkdir" ]; then
+    arg=$(cat .curworkdir)
+fi
+
+git submodule update --init $arg
 git submodule foreach -q --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
